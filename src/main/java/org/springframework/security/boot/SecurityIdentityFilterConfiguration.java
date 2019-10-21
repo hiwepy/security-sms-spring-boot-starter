96b9951a11d2f5cc956053bb9e4399f1527567b6
wandl <hnxyhcwdl1003@163.com>
2019-10-21 20:18:04 +0800

    逻辑优化

--- src/main/java/org/springframework/security/boot/SecurityIdentityFilterConfiguration.java
+++ src/main/java/org/springframework/security/boot/SecurityIdentityFilterConfiguration.java
@@ -1,5 +1,7 @@
 package org.springframework.security.boot;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
@@ -11,6 +13,7 @@ import org.springframework.boot.context.properties.PropertyMapper;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.boot.biz.authentication.PostRequestAuthenticationFailureHandler;
 import org.springframework.security.boot.biz.authentication.PostRequestAuthenticationSuccessHandler;
 import org.springframework.security.boot.identity.authentication.IdentityCodeAuthenticationProcessingFilter;
@@ -77,7 +80,16 @@ public class SecurityIdentityFilterConfiguration {
    			
    		}
    		
-   	    public IdentityCodeAuthenticationProcessingFilter authenticationProcessingFilter() {
+   		@Override
+		public AuthenticationManager authenticationManagerBean() throws Exception {
+   			AuthenticationManager parentManager = authenticationManager == null ? super.authenticationManagerBean() : authenticationManager;
+			ProviderManager authenticationManager = new ProviderManager( Arrays.asList(authenticationProvider), parentManager);
+			// 不擦除认证密码，擦除会导致TokenBasedRememberMeServices因为找不到Credentials再调用UserDetailsService而抛出UsernameNotFoundException
+			authenticationManager.setEraseCredentialsAfterAuthentication(false);
+			return authenticationManager;
+		}
+   		
+   	    public IdentityCodeAuthenticationProcessingFilter authenticationProcessingFilter() throws Exception {
    	    	
    			IdentityCodeAuthenticationProcessingFilter authenticationFilter = new IdentityCodeAuthenticationProcessingFilter(
    					objectMapper);
@@ -89,7 +101,7 @@ public class SecurityIdentityFilterConfiguration {
 			
 			map.from(bizProperties.getSessionMgt().isAllowSessionCreation()).to(authenticationFilter::setAllowSessionCreation);
 			
-			map.from(authenticationManager).to(authenticationFilter::setAuthenticationManager);
+			map.from(authenticationManagerBean()).to(authenticationFilter::setAuthenticationManager);
 			map.from(authenticationSuccessHandler).to(authenticationFilter::setAuthenticationSuccessHandler);
 			map.from(authenticationFailureHandler).to(authenticationFilter::setAuthenticationFailureHandler);
 			

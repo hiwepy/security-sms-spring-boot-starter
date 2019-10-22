b0e40cd3437a138e01021d503aca60f5b44608b9
wandl <hnxyhcwdl1003@163.com>
2019-10-22 19:40:50 +0800

    xd

--- src/main/java/org/springframework/security/boot/SecurityIdentityFilterConfiguration.java
+++ src/main/java/org/springframework/security/boot/SecurityIdentityFilterConfiguration.java
@@ -1,9 +1,9 @@
 package org.springframework.security.boot;
 
-import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
@@ -13,18 +13,26 @@ import org.springframework.boot.context.properties.PropertyMapper;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.boot.biz.authentication.PostRequestAuthenticationFailureHandler;
-import org.springframework.security.boot.biz.authentication.PostRequestAuthenticationSuccessHandler;
+import org.springframework.security.boot.biz.authentication.AuthenticationListener;
+import org.springframework.security.boot.biz.authentication.captcha.CaptchaResolver;
+import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationEntryPoint;
+import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationFailureHandler;
+import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationSuccessHandler;
 import org.springframework.security.boot.identity.authentication.IdentityCodeAuthenticationProcessingFilter;
 import org.springframework.security.boot.identity.authentication.IdentityCodeAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.RememberMeServices;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.session.InvalidSessionStrategy;
+import org.springframework.security.web.session.SessionInformationExpiredStrategy;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -39,58 +47,59 @@ public class SecurityIdentityFilterConfiguration {
     @Order(SecurityProperties.DEFAULT_FILTER_ORDER + 4)
    	static class IdentityWebSecurityConfigurerAdapter extends SecurityBizConfigurerAdapter {
     	
-        private final AuthenticationManager authenticationManager;
-	    private final ObjectMapper objectMapper;
-	    private final RememberMeServices rememberMeServices;
-	    private final SecurityBizProperties bizProperties;
     	private final SecurityIdentityAuthcProperties authcProperties;
-    	private final IdentityCodeAuthenticationProvider authenticationProvider;
-    	private final PostRequestAuthenticationSuccessHandler authenticationSuccessHandler;
- 	    private final PostRequestAuthenticationFailureHandler authenticationFailureHandler;
-	    
-		private final SessionAuthenticationStrategy sessionAuthenticationStrategy;
+    	
+    	private final AuthenticationEntryPoint authenticationEntryPoint;
+ 	    private final AuthenticationSuccessHandler authenticationSuccessHandler;
+ 	    private final AuthenticationFailureHandler authenticationFailureHandler;
+ 	    private final CaptchaResolver captchaResolver;
+ 	    private final InvalidSessionStrategy invalidSessionStrategy;
+ 	    private final LogoutHandler logoutHandler;
+ 	    private final ObjectMapper objectMapper;
+     	private final RequestCache requestCache;
+     	private final RememberMeServices rememberMeServices;
+     	private final SessionRegistry sessionRegistry;
+ 		private final SessionAuthenticationStrategy sessionAuthenticationStrategy;
+ 		private final SessionInformationExpiredStrategy sessionInformationExpiredStrategy;
    		
    		public IdentityWebSecurityConfigurerAdapter(
    			
    				SecurityBizProperties bizProperties,
-   				SecurityIdentityAuthcProperties identityAuthcProperties,
+   				SecurityIdentityAuthcProperties authcProperties,
 
-   				ObjectProvider<AuthenticationManager> authenticationManagerProvider,
    				ObjectProvider<IdentityCodeAuthenticationProvider> authenticationProvider,
-   				ObjectProvider<IdentityCodeAuthenticationProcessingFilter> authenticationProcessingFilter,
-   				ObjectProvider<CsrfTokenRepository> csrfTokenRepositoryProvider,
-   				ObjectProvider<ObjectMapper> objectMapperProvider,
-   				ObjectProvider<PostRequestAuthenticationFailureHandler> authenticationFailureHandler,
-   				ObjectProvider<RememberMeServices> rememberMeServicesProvider,
-				ObjectProvider<SessionAuthenticationStrategy> sessionAuthenticationStrategyProvider,
-				
-				@Qualifier("idcAuthenticationSuccessHandler") ObjectProvider<PostRequestAuthenticationSuccessHandler> authenticationSuccessHandler
+   				ObjectProvider<AuthenticationManager> authenticationManagerProvider,
+   				ObjectProvider<AuthenticationListener> authenticationListenerProvider,
+   				ObjectProvider<MatchedAuthenticationEntryPoint> authenticationEntryPointProvider,
+   				ObjectProvider<MatchedAuthenticationSuccessHandler> authenticationSuccessHandlerProvider,
+   				ObjectProvider<MatchedAuthenticationFailureHandler> authenticationFailureHandlerProvider,
+   				ObjectProvider<CaptchaResolver> captchaResolverProvider,
+   				ObjectProvider<LogoutHandler> logoutHandlerProvider,
+   				ObjectProvider<ObjectMapper> objectMapperProvider
+
 			) {
    			
-   			super(bizProperties, csrfTokenRepositoryProvider.getIfAvailable());
+   			super(bizProperties, authcProperties, authenticationProvider.stream().collect(Collectors.toList()),
+					authenticationManagerProvider.getIfAvailable());
    			
-   			this.authenticationManager = authenticationManagerProvider.getIfAvailable();
+   			this.authcProperties = authcProperties;
+   			
+   			List<AuthenticationListener> authenticationListeners = authenticationListenerProvider.stream().collect(Collectors.toList());
+   			this.authenticationEntryPoint = super.authenticationEntryPoint(authenticationEntryPointProvider.stream().collect(Collectors.toList()));
+   			this.authenticationSuccessHandler = super.authenticationSuccessHandler(authenticationListeners, authenticationSuccessHandlerProvider.stream().collect(Collectors.toList()));
+   			this.authenticationFailureHandler = super.authenticationFailureHandler(authenticationListeners, authenticationFailureHandlerProvider.stream().collect(Collectors.toList()));
+   			this.captchaResolver = captchaResolverProvider.getIfAvailable();
+   			this.invalidSessionStrategy = super.invalidSessionStrategy();
+   			this.logoutHandler = super.logoutHandler(logoutHandlerProvider.stream().collect(Collectors.toList()));
    			this.objectMapper = objectMapperProvider.getIfAvailable();
-   			this.rememberMeServices = rememberMeServicesProvider.getIfAvailable();
-   			this.bizProperties = bizProperties;
-   			this.authcProperties = identityAuthcProperties;
-   			this.authenticationProvider = authenticationProvider.getIfAvailable();
-   			this.authenticationSuccessHandler = authenticationSuccessHandler.getIfAvailable();
-   			this.authenticationFailureHandler = authenticationFailureHandler.getIfAvailable();
-   			
-   			this.sessionAuthenticationStrategy = sessionAuthenticationStrategyProvider.getIfAvailable();
+   			this.requestCache = super.requestCache();
+   			this.rememberMeServices = super.rememberMeServices();
+   			this.sessionRegistry = super.sessionRegistry();
+   			this.sessionAuthenticationStrategy = super.sessionAuthenticationStrategy();
+   			this.sessionInformationExpiredStrategy = super.sessionInformationExpiredStrategy();
    			
    		}
-   		
-   		@Override
-		public AuthenticationManager authenticationManagerBean() throws Exception {
-   			AuthenticationManager parentManager = authenticationManager == null ? super.authenticationManagerBean() : authenticationManager;
-			ProviderManager authenticationManager = new ProviderManager( Arrays.asList(authenticationProvider), parentManager);
-			// 不擦除认证密码，擦除会导致TokenBasedRememberMeServices因为找不到Credentials再调用UserDetailsService而抛出UsernameNotFoundException
-			authenticationManager.setEraseCredentialsAfterAuthentication(false);
-			return authenticationManager;
-		}
-   		
+   		   		
    	    public IdentityCodeAuthenticationProcessingFilter authenticationProcessingFilter() throws Exception {
    	    	
    			IdentityCodeAuthenticationProcessingFilter authenticationFilter = new IdentityCodeAuthenticationProcessingFilter(
@@ -101,7 +110,7 @@ public class SecurityIdentityFilterConfiguration {
 			 */
 			PropertyMapper map = PropertyMapper.get().alwaysApplyingWhenNonNull();
 			
-			map.from(bizProperties.getSessionMgt().isAllowSessionCreation()).to(authenticationFilter::setAllowSessionCreation);
+			map.from(authcProperties.getSessionMgt().isAllowSessionCreation()).to(authenticationFilter::setAllowSessionCreation);
 			
 			map.from(authenticationManagerBean()).to(authenticationFilter::setAuthenticationManager);
 			map.from(authenticationSuccessHandler).to(authenticationFilter::setAuthenticationSuccessHandler);
@@ -116,12 +125,6 @@ public class SecurityIdentityFilterConfiguration {
 			map.from(authcProperties.isContinueChainBeforeSuccessfulAuthentication()).to(authenticationFilter::setContinueChainBeforeSuccessfulAuthentication);
 			
    	        return authenticationFilter;
-   	    }
-   		
-   		@Override
-		public void configure(AuthenticationManagerBuilder auth) throws Exception {
-   	        auth.authenticationProvider(authenticationProvider);
-   	        super.configure(auth);
    	    }
 
    	    @Override
